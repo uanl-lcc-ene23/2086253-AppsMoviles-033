1fedae63e0a61139ca9428b199c1e2876c60ca17
Emilio Madrid <84986154+EmilioMadrid@users.noreply.github.com>
2023-04-01 23:26:48 -0600

    Data Binding y Comunicación de Componentes

--- ticket-ionic/src/app/ticket/ticket.page.ts
+++ ticket-ionic/src/app/ticket/ticket.page.ts
@@ -7,9 +7,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TicketPage implements OnInit {
 
+  articulos: any[] = [];
+  articulo: any = { descripcion: '', precio: '' };
+  total: number = 0;
+
+  mostrarCaja: boolean = false;
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  agregarArticulo() {
+    this.articulos.push(this.articulo);
+    this.calcularTotal();
+    this.articulo = { descripcion: '', precio: '' };
+  }
+
+  calcularTotal() {
+    let total = 0;
+    for (let i = 0; i < this.articulos.length; i++) {
+      total += parseFloat(this.articulos[i].precio);
+    }
+    this.total = total;
+
+    this.mostrarCaja = true;
+    setTimeout(() => {
+      this.mostrarCaja = false;
+    }, 3000);
+  }
+
+  cerrarCaja() {
+    this.mostrarCaja = false;
+  }  
 }
